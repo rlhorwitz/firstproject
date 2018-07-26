1c18ed3caa7fd2309f2b6e40ccc899a3d3304856
Rachel <rachel@Joshuas-MacBook-Pro.local>
2018-07-25 23:18:07 -0400

    Pyramid Exercise

---
+++ Exercises/Pyramid.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void) {
+
+    int height;
+
+    // Do-While Loop
+    do {
+        printf("Height: ");
+        height = get_int();
+    } while ( height < 0 || height > 23);
+    // For loop to print the pyramid
+    for (int i = 1; i <= height; i++) {
+        for (int p = 1; p <= height-i; p++ ) {
+            printf(" ");
+        }
+        for (int h = 1; h <= i + 1; h++){
+            printf("#");
+        }
+        printf("\n");
+    }
+}
